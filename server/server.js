c8c84369f52ad128f47dde8c4f2b374451e333d4
kennysabangan <61653677+kennysabangan@users.noreply.github.com>
2022-03-22 20:04:36 -0700

    Initialize Project

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 8000;
+const cookieParser = require('cookie-parser');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+
+require('./config/mongoose.config');
+require('dotenv').config();
+
+app.listen(port, ()=> console.log(`Listening on port: ${port}`));
