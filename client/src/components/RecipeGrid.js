9ff08574919cb2adbcda62cf5ff8fd9e167e48e2
Austin Nye <amnye0815@gmail.com>
2022-03-25 13:14:48 -0700

    card with ripple effect test

--- client/src/components/RecipeGrid.js
+++ client/src/components/RecipeGrid.js
@@ -1,60 +1,24 @@
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem } from "mdbreact";
+import React from 'react';
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
 
-const Navigation = () => {
-
-    const navigate = useNavigate();
-
-    const logout = () => {
-        axios.get('http://localhost:8000/api/users/logout', { withCredentials: true })
-            .then(() => navigate('/'))
-            .catch(err => console.log(err))
-    }
-
+const RecipeGrid() {
     return (
-        // <!-- Navbar -->
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            {/* <!-- Container wrapper --> */}
-            <div className="container">
-                {/* <!-- Navbar brand --> */}
-                <a className="navbar-brand me-2" href="/dashboard">
-                <img
-                    src="food-favicon.png"
-                    height="30"
-                    loading="lazy"
-                />
+        <MDBCard style={{ maxWidth: '22rem' }}>
+            <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+                <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/111.webp' fluid alt='...' />
+                <a>
+                    <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
                 </a>
-
-                {/* <!-- Left links --> */}
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <a className="nav-link" href="/dashboard">Dashboard</a>
-                    </li>
-                    <li className="nav-item px-2">
-                        <a className="nav-link" href="/meals">Find Random Meal</a>
-                    </li>
-                </ul>
-
-                {/* <!-- Right links --> */}
-                <div className="d-flex align-items-center">
-                    <MDBDropdown>
-                        <MDBDropdownToggle caret color="light">
-                            <img src={require('../static/no-profile.png')} className="me-2" height="20px"/>
-                            My Profile
-                        </MDBDropdownToggle>
-                        <MDBDropdownMenu basic>
-                            <MDBDropdownItem href="/profile">View Profile</MDBDropdownItem>
-                            <MDBDropdownItem href="/recipes">View Recipes</MDBDropdownItem>
-                        </MDBDropdownMenu>
-                    </MDBDropdown>
-                    <button onClick={logout} type="button" className="btn btn-primary ms-3 me-2">
-                        Logout
-                    </button>
-                </div>
-            </div>
-        </nav>
-    )
+            </MDBRipple>
+            <MDBCardBody>
+                <MDBCardTitle>Recipe Title</MDBCardTitle>
+                <MDBCardText>
+                    Short description of recipe
+                </MDBCardText>
+                <MDBBtn href='#'>Add to 'My Recipes'</MDBBtn>
+            </MDBCardBody>
+        </MDBCard>      
+    );
 }
 
-export default Navigation;
+export default RecipeGrid;
