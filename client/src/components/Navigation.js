264b8a88dec18d51b47b9486006e9973f8648a74
kennysabangan <61653677+kennysabangan@users.noreply.github.com>
2022-03-24 22:43:37 -0700

    update Navigation

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem } from "mdbreact";
 
 const Navigation = () => {
 
@@ -14,64 +16,43 @@ const Navigation = () => {
     return (
         // <!-- Navbar -->
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        {/* <!-- Container wrapper --> */}
-        <div className="container">
-            {/* <!-- Navbar brand --> */}
-            <a className="navbar-brand me-2" href="/dashboard">
-            <img
-                src="food-favicon.png"
-                height="30"
-                loading="lazy"
-            />
-            </a>
-
-            {/* <!-- Toggle button --> */}
-            <button
-                className="navbar-toggler"
-                type="button"
-                data-mdb-toggle="collapse"
-                data-mdb-target="#navbarButtonsExample"
-                aria-controls="navbarButtonsExample"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-                >
-            <i className="fas fa-bars"></i>
-            </button>
-
+            {/* <!-- Container wrapper --> */}
+            <div className="container">
+                {/* <!-- Navbar brand --> */}
+                <a className="navbar-brand me-2" href="/dashboard">
+                <img
+                    src="food-favicon.png"
+                    height="30"
+                    loading="lazy"
+                />
+                </a>
 
                 {/* <!-- Left links --> */}
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <a className="nav-link" href="/dashboard">Dashboard</a>
+                        <a className="nav-link" href="/dashboard">Dashboard</a>
+                    </li>
+                    <li className="nav-item px-2">
+                        <a className="nav-link" href="/meals">Find Random Meal</a>
                     </li>
                 </ul>
 
                 {/* <!-- Right links --> */}
                 <div className="d-flex align-items-center">
-                    <div className="dropdown">
-                        <a
-                            className="btn btn-link dropdown-toggle"
-                            href="#"
-                            role="button"
-                            id="dropdownMenuLink"
-                            data-mdb-toggle="dropdown"
-                            aria-expanded="false"
-                        >
-                        <img src={require('../static/no-profile.png')} className="me-2" height="20px"/>
-                        My Profile
-                        </a>
-
-                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                            <li><a className="dropdown-item" href="#">Action</a></li>
-                            <li><a className="dropdown-item" href="#">Another action</a></li>
-                            <li><a className="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </div>
-                    <button onClick={logout} type="button" className="btn btn-link ms-4 px-3 me-2">
+                    <MDBDropdown>
+                        <MDBDropdownToggle caret color="light">
+                            <img src={require('../static/no-profile.png')} className="me-2" height="20px"/>
+                            My Profile
+                        </MDBDropdownToggle>
+                        <MDBDropdownMenu basic>
+                            <MDBDropdownItem href="/profile">View Profile</MDBDropdownItem>
+                            <MDBDropdownItem href="/recipes">View Recipes</MDBDropdownItem>
+                        </MDBDropdownMenu>
+                    </MDBDropdown>
+                    <button onClick={logout} type="button" className="btn btn-primary ms-3 me-2">
                         Logout
                     </button>
                 </div>
-
             </div>
         </nav>
     )
