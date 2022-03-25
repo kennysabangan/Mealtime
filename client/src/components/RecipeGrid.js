5697a1026f2be8e12ac9cdbe773880c291df83d3
Austin Nye <amnye0815@gmail.com>
2022-03-25 13:47:14 -0700

    updating

--- client/src/components/RecipeGrid.js
+++ client/src/components/RecipeGrid.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple, MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 
 const RecipeGrid = () => {
     return (
-        <MDBCard style={{ maxWidth: '22rem' }}>
-            <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
-                <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/111.webp' fluid alt='...' />
-                <a>
-                    <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
-                </a>
-            </MDBRipple>
-            <MDBCardBody>
-                <MDBCardTitle>Recipe Title</MDBCardTitle>
-                <MDBCardText>
-                    Short description of recipe
-                </MDBCardText>
-                <MDBBtn style={{ backgroundColor: "#48BD8F" }} href='#'>Add to 'My Recipes'</MDBBtn>
-            </MDBCardBody>
-        </MDBCard>      
+        <MDBContainer style={{ padding: '10rem'}}>
+            <MDBCard style={{ maxWidth: '22rem' }}>
+                <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+                    <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/111.webp' fluid alt='...' />
+                    <a>
+                        <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+                    </a>
+                </MDBRipple>
+                <MDBCardBody>
+                    <MDBCardTitle>Recipe Title</MDBCardTitle>
+                    <MDBCardText>
+                        Short description of recipe
+                    </MDBCardText>
+                    <MDBBtn style={{ backgroundColor: "#48BD8F" }} href='#'>Add to 'My Recipes'</MDBBtn>
+                </MDBCardBody>
+            </MDBCard>
+        </MDBContainer>     
     );
 }
 
