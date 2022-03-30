937632cd7c5505911535e7b2261b545f9557afb9
kennysabangan <61653677+kennysabangan@users.noreply.github.com>
2022-03-29 17:05:33 -0700

    Search Now uses Recipe Search API

--- client/src/components/ChooseAMeal.js
+++ client/src/components/ChooseAMeal.js
@@ -3,85 +3,175 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const ChooseAMeal = (props) => {
-  const { tags } = props;
-  const [meals, setMeals] = useState([]);
+  const { tags, query } = props;
+  const [meals, setMeals] = useState();
   const [index, setIndex] = useState(0);
   const [meal, setMeal] = useState({});
   const [loaded, setLoaded] = useState(false);
   const joinedTags = tags.join(", ");
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const getMeals = async () => {
-      try {
-        // Random query if tags ARE given
-        if (tags.length != 0) {
+  const getMeals = async () => {
+    try {
+      // If a Search Query IS Given
+      if (query) {
+        // OUR TAGS: dairy free, vegan, grain free, keto, whole30
+        // diets possible: pescetarian, lacto vegetarian, ovo vegetarian, vegan, and vegetarian.
+        // intolerances possible: dairy, egg, gluten, peanut, sesame, seafood, shellfish, soy, sulfite, tree nut, and wheat.
+
+        // >>> HOW WE'LL USE OUR TAGS <<<
+        // dairy free: { intolerance: dairy }
+        // vegan: { diet: vegan }
+        // grain free: { intolerance: gluten, wheat }
+        // keto: { } - can't account for keto, maybe let's try { intolerance: wheat}
+        // whole30: { diet: ovo vegetarian, intolerance: dairy, gluten }
+
+        let dietQuery = [], intolerancesQuery = [];
+        tags.map(tag => {
+          switch(tag) {
+            case "dairy free":
+              if (dietQuery.length == 0) {
+                intolerancesQuery.push('dairy');
+              }
+              break;
+            case "vegan":
+              if (dietQuery.length == 0) {
+                dietQuery.push('vegan')
+              }
+              break;
+            case "grain free":
+              if (!intolerancesQuery.includes('gluten')) {
+                intolerancesQuery.push('gluten');
+              }
+              if (!intolerancesQuery.includes('wheat')) {
+                intolerancesQuery.push('wheat');
+              }
+              break;
+            case "keto":
+              if (!intolerancesQuery.includes('wheat')) {
+                intolerancesQuery.push('wheat');
+              }
+              break;
+            case "whole30":
+              if (dietQuery.length == 0) {
+                dietQuery.push('ovo vegetarian')
+              }
+              if (!intolerancesQuery.includes('dairy')) {
+                intolerancesQuery.push('dairy')
+              }
+              if (!intolerancesQuery.includes('gluten')) {
+                intolerancesQuery.push('gluten')
+              }
+              break;
+            }
+          })
+          const dietString = dietQuery.length != 0 ? dietQuery.join(',') : ' ';
+          const intolerancesString = intolerancesQuery.length != 0 ? intolerancesQuery.join(',') : ' ';
+          // console.log(dietString, intolerancesString)
+
+          var options = {
+          method: 'GET',
+          url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search',
+          params: {
+            query: `${ query }`,
+            diet: `${ dietString }`,
+            intolerances: `${ intolerancesString }`,
+            instructionsRequired: 'true',
+            number: '1',
+            offset: '0',
+          },
+          headers: {
+            'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+            'X-RapidAPI-Key': '9fc53f2c2fmsh8633a9448fc45adp1d4bd0jsn4d61359145cd'
+          }
+        };
+        // console.log("options: ", options);
+
+      } else if (tags.length != 0) {
+        // No Query => Run "Random" query if tags ARE given
+        var options = {
+          method: "GET",
+          url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random",
+          params: { tags: { joinedTags }, number: "2" },
+          headers: {
+            "X-RapidAPI-Host":
+              "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            "X-RapidAPI-Key":
+              "9fc53f2c2fmsh8633a9448fc45adp1d4bd0jsn4d61359145cd",
+          },
+        };
+      } else {
+        // No Query => Run "Random" query BUT IF tags ARE NOT given
+        var options = {
+          method: "GET",
+          url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random",
+          params: { number: "2" },
+          headers: {
+            "X-RapidAPI-Host":
+              "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            "X-RapidAPI-Key":
+              "9fc53f2c2fmsh8633a9448fc45adp1d4bd0jsn4d61359145cd",
+          },
+        };
+      }
+
+      let res = await axios.request(options);
+
+      // Makes a Bing Image Search for every recipe since original Recipe API's photos were very poor quality
+      const pointer = res.data.expires ? res.data.results : res.data.recipes;
+      const replaceImages = async () => {
+        pointer.forEach(async (recipe) => {
+          if (query) {
+            recipe.image = res.data.baseUri + recipe.image
+          }
           var options = {
             method: "GET",
-            url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random",
-            params: { tags: { joinedTags }, number: "2" },
+            url: "https://bing-image-search1.p.rapidapi.com/images/search",
+            params: { q: `${recipe.title} recipe`, count: "1", offset: "1" },
             headers: {
-              "X-RapidAPI-Host":
-                "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+              "X-RapidAPI-Host": "bing-image-search1.p.rapidapi.com",
               "X-RapidAPI-Key":
                 "9fc53f2c2fmsh8633a9448fc45adp1d4bd0jsn4d61359145cd",
             },
           };
-        } else {
-          // Random query if tages ARE NOT given
-          var options = {
-            method: "GET",
-            url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random",
-            params: { number: "2" },
-            headers: {
-              "X-RapidAPI-Host":
-                "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-              "X-RapidAPI-Key":
-                "0f84236229msh09b7a39c03b9eafp13e306jsnb8eba2675d6a",
-            },
-          };
-        }
 
-        const res = await axios.request(options);
-        console.log(res.data);
-        const apiData = res.data;
-
-        // Makes a Bing Image Search for every recipe since original Recipe API's photos were very poor quality
-        res.data.recipes.forEach((recipe) => {
-          var options = {
-            method: "GET",
-            url: "https://bing-image-search1.p.rapidapi.com/images/search",
-            params: { q: `${recipe.title} recipe`, count: "3", offset: "1" },
-            headers: {
-              "X-RapidAPI-Host": "bing-image-search1.p.rapidapi.com",
-              "X-RapidAPI-Key":
-                "0f84236229msh09b7a39c03b9eafp13e306jsnb8eba2675d6a",
-            },
-          };
-          axios
+          await axios
             .request(options)
             .then(function (response) {
               response.data.value[0].thumbnailUrl
                 ? (recipe.image = response.data.value[0].thumbnailUrl)
-                : (recipe.image = recipe.image);
-            })
-            .catch(function (error) {
-              console.error(error);
-            });
+                : (recipe.image);
+              })
+              .catch(function (error) {
+                console.error(error);
+              });
         });
 
-        setMeals(apiData);
-        setMeal(res.data.recipes[0]);
-        setLoaded(true);
-        console.log(res.data);
-      } catch (error) {
-        console.log(error);
+        // Search Query returns 'Results' but our original codebase calls for apiData['recipes']
+        if (res.data.results) {
+          res.data.recipes = res.data.results;
+          delete res.data.results;
+        }
+        return await res.data
       }
-    };
-    getMeals();
+      const apiData = await replaceImages();
+      return apiData;
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(async () => {
+    var loadedApiData = await getMeals();
+    await setMeal(loadedApiData.recipes[0])
+    await setMeals(loadedApiData)
+    await setLoaded(true);
   }, []);
 
   const handleNext = () => {
+    console.log('index', index);
+    console.log(meals);
     let tempIndex = index;
     if (tempIndex < meals.recipes.length - 1) {
       tempIndex = tempIndex + 1;
@@ -123,7 +213,7 @@ const ChooseAMeal = (props) => {
       });
   };
 
-  return meals.recipes && meals.recipes.length != 0 ? (
+  return loaded && meal ? (
     <div className="container">
       <div className="row text-center d-flex justify-content-center align-items-center">
         <h1 className="tasty cursive mt-5 rounded" style={{ width: 415 }}>
@@ -137,7 +227,7 @@ const ChooseAMeal = (props) => {
         >
           <button
             className={`meal-btn d-flex gap-3 align-items-center justify-content-center pt-3 ${
-              meals.recipes && index == 0 ? "disabled" : null
+              meals && index == 0 ? "disabled" : null
             }`}
           >
             <i className="fas fa-angle-left mb-2 pb-1 "></i>
@@ -149,17 +239,17 @@ const ChooseAMeal = (props) => {
             <div className="bg-image d-flex justify-content-center">
               {/* <img src="https://media.olivegarden.com/en_us/images/product/classic-chicken-alfredo-dinner-dpv-590x365.jpg" className="img-fluid"/> */}
               <img
-                src={meals.recipes && meals.recipes[index].image}
+                src={meals && meal.image}
                 height="420"
               />
             </div>
             <div className="card-body text-center">
-              <h2>{meals.recipes && meals.recipes[index].title}</h2>
+              <h2>{meals && meal.title}</h2>
               <p className="card-text">
-                Total Servings: {meals.recipes && meals.recipes[index].servings}
+                Total Servings: {meals && meal.servings}
               </p>
               <p className="card-text">
-                Ready in: {meals.recipes && meals.recipes[index].readyInMinutes}{" "}
+                Ready in: {meals && meal.readyInMinutes}{" "}
                 minutes
               </p>
             </div>
@@ -171,7 +261,7 @@ const ChooseAMeal = (props) => {
         >
           <button
             className={`meal-btn d-flex gap-3 align-items-center justify-content-center pt-3 ${
-              meals.recipes && index == meals.recipes.length - 1
+              meals && index == meals.recipes.length - 1
                 ? "disabled"
                 : null
             }`}
@@ -198,7 +288,7 @@ const ChooseAMeal = (props) => {
   ) : (
     loaded && (
       // If No Search Query Found, Return 404 Message
-      <div className="text-center mt-5 pt-5">
+      <div className="text-center" style={{ paddingTop: "350px" }}>
         <h1 className="cursive">
           We were not able to find a recipe with your search.
         </h1>
