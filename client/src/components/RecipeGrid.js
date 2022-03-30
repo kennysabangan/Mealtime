348c23793a58d3ea538f0d17795f8346649fa44d
Alan Nix <44075306+NixAlan@users.noreply.github.com>
2022-03-30 03:07:20 -0500

    merge to main

--- client/src/components/RecipeGrid.js
+++ client/src/components/RecipeGrid.js
@@ -1,39 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBRipple, MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBRipple,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+} from "mdb-react-ui-kit";
 
 const RecipeGrid = () => {
-    const [recipes, setRecipes] = useState([]);
-    const [loaded, setLoaded] = useState(true);
+  const [recipes, setRecipes] = useState([]);
+  const [loaded, setLoaded] = useState(true);
 
-    useEffect(() => {
-        const getRecipes = async () => {
-            const res = await axios.get('http://localhost:8000/api/users/recipes', { withCredentials: true })
-            setRecipes(res.data)
-          };
-          getRecipes();
-          console.log(recipes);
-    }, [])
+  useEffect(() => {
+    const getRecipes = async () => {
+      const res = await axios.get("http://localhost:8000/api/users/recipes", {
+        withCredentials: true,
+      });
+      setRecipes(res.data);
+    };
+    getRecipes();
+    console.log(recipes);
+  }, []);
 
-    // const Display = (props) => {
-    //     const {id} = props;
-    //     const [petList, setPetList] = useState([]); -----but for recipe list   -------
+  // const Display = (props) => {
+  //     const {id} = props;
+  //     const [petList, setPetList] = useState([]); -----but for recipe list   -------
 
+  //     useEffect(() => {
+  //         axios.get('http://localhost:8000/api/pets') ------ our API of choice --------
+  //             .then(res=> {
+  //                 console.log(res);
+  //                 console.log(res.data);
+  //                 setPetList(res.data);
+  //             })
+  //             .catch((err)=> {
+  //                 console.log(err);
+  //                 navigate("/error");
+  //             })
+  //     }, [id])
 
-    //     useEffect(() => {
-    //         axios.get('http://localhost:8000/api/pets') ------ our API of choice --------
-    //             .then(res=> {
-    //                 console.log(res);
-    //                 console.log(res.data);
-    //                 setPetList(res.data);
-    //             })
-    //             .catch((err)=> {
-    //                 console.log(err);
-    //                 navigate("/error");
-    //             })
-    //     }, [id])
-
-    {/* {
+  {
+    /* {
                 petList? ------ but for recipeList -------
 
                 petList.map((pet, index) => (
@@ -47,28 +59,39 @@ const RecipeGrid = () => {
                     </tr>
                 ))
                 :null
-            } */}
+            } */
+  }
 
-    return (
-        <MDBContainer className="d-flex flex-wrap gap-2" >
-            { recipes.map((item, idx) => (
-            <MDBCard key={idx} className="m-2" style={{ width: "30%"}}>
-                <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay text-center'>
-                    <a href={`/onerecipe/${item.recipe._id}`}>
-                        <MDBCardImage src={item.recipe.image} height="225" />
-                    </a>
-                </MDBRipple>
-                <MDBCardBody>
-                    <MDBCardTitle>{item.recipe.recipeName}</MDBCardTitle>
-                    <MDBCardText>
-                        <span>Servings: {item.recipe.servings} <i className="fas fa-utensils mx-2"></i> Time to Cook: {item.recipe.prepTime} minutes </span>
-                    </MDBCardText>
-                </MDBCardBody>
-            </MDBCard>
-
-            ))}
-        </MDBContainer>
-    );
-}
+  return (
+    <MDBContainer className="d-flex flex-wrap gap-2">
+      {recipes.map((item, idx) => (
+        <MDBCard key={idx} className="m-2" style={{ width: "30%" }}>
+          <MDBRipple
+            rippleColor="light"
+            rippleTag="div"
+            className="bg-image hover-overlay text-center"
+          >
+            <MDBCardImage src={item.recipe.image} height="225" />
+            <a href="/onerecipe">
+              <div
+                style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
+              ></div>
+            </a>
+          </MDBRipple>
+          <MDBCardBody>
+            <MDBCardTitle>{item.recipe.recipeName}</MDBCardTitle>
+            <MDBCardText>
+              <span>
+                Servings: {item.recipe.servings}{" "}
+                <i className="fas fa-utensils mx-2"></i> Time to Cook:{" "}
+                {item.recipe.prepTime} minutes{" "}
+              </span>
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      ))}
+    </MDBContainer>
+  );
+};
 
 export default RecipeGrid;
