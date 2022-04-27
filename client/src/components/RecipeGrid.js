1441b71c68b04b3b3567b13a21fce7e1538539c8
Austin Nye <amnye0815@gmail.com>
2022-04-27 08:43:11 -0700

    Update RecipeGrid.js

--- client/src/components/RecipeGrid.js
+++ client/src/components/RecipeGrid.js
@@ -23,40 +23,6 @@ const RecipeGrid = (props) => {
     getRecipes();
   }, []);
 
-  // const Display = (props) => {
-  //     const {id} = props;
-  //     const [petList, setPetList] = useState([]); -----but for recipe list   -------
-
-  //     useEffect(() => {
-  //         axios.get('http://localhost:8000/api/pets') ------ our API of choice --------
-  //             .then(res=> {
-  //                 console.log(res);
-  //                 console.log(res.data);
-  //                 setPetList(res.data);
-  //             })
-  //             .catch((err)=> {
-  //                 console.log(err);
-  //                 navigate("/error");
-  //             })
-  //     }, [id])
-
-  {
-    /* {
-                petList? ------ but for recipeList -------
-
-                petList.map((pet, index) => (
-                    <tr key={index}>
-                        <td style={{borderRight:"1px solid black"}}>{pet.petName}</td>
-                        <td style={{borderRight:"1px solid black"}}>{pet.petType}</td>
-                        <td style={{display:"flex"}}>
-                            <Link to={`/pets/${pet._id}`} style={{borderRight:"1px solid black", paddingRight:"10px"}}>details</Link>
-                            <Link to={`/pets/${pet._id}/edit`} style={{marginLeft:"5px"}}>edit</Link>
-                        </td>
-                    </tr>
-                ))
-                :null
-            } */
-  }
 
   return (
     <MDBContainer className="d-flex flex-wrap gap-2">
