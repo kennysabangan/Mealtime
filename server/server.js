4c97a0c39a8f5ea073a2a772831d3444c5d039fd
Kenny Sabangan <ksabangan121@gmail.com>
2022-08-17 00:35:44 -0700

    edit server config

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 8000;
+const port = process.env.PORT || 80;
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
